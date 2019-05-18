fe7309b78af6a0c55f6123e642c3f4323adbe23e
Midas <vanhoeymidas@gmail.com>
2019-05-18 13:11:11 +0200

    Observable users

---
+++ UserApp/src/app/mock-users.ts
@@ -0,0 +1,21 @@
+import { User } from './User';
+export const USERS: User[] = [
+    {
+        id: "jan@ucll.be",
+        firstName: "Jan",
+        lastName: "Janssens",
+        status: "offline"
+    },
+    {
+        id: "an@ucll.be",
+        firstName: "An",
+        lastName: "Anderson",
+        status: "offline"
+    },
+    {
+        id: "bib@ucll.be",
+        firstName: "Bib",
+        lastName: "Liotheek",
+        status: "offline"
+    }
+]
